d7d443b1005c4260772f4ec44f7dfdca0b9d3219
Josh Daniel <joshdaniel@Mac.lan>
2026-03-18 21:24:54 -0400

    Test resource hub component

--- src/views/ResourcesHubView.vue
+++ src/views/ResourcesHubView.vue
@@ -1,1 +1,5 @@
-<template><div style="color:white;padding:40px;background:#05060a;min-height:100vh"><h1>Test - routing works</h1></div></template>
+<template>
+  <div style="color:white;padding:40px;background:#05060a;min-height:100vh">
+    <h1 style="font-size:32px">Resources - routing works!</h1>
+  </div>
+</template>
