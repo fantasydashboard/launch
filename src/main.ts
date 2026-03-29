9c28748a1a4333d2681ba0aab13e0d3dd7a6517a
Josh Daniel <joshdaniel@Mac.lan>
2026-03-28 21:27:01 -0400

    Add Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import VueApexCharts from 'vue3-apexcharts'
 import App from './App.vue'
 import router from './router'
 import './style.css'
+import { inject } from '@vercel/analytics'  // ← add line 7
 
 const app = createApp(App)
+
+inject()  // ← add after this blank line
 
 app.use(createPinia())
 app.use(router)
